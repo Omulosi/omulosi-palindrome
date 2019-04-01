63f3aa250f3a11c1e373640d9c0279bafa529a5d
Omulosi <mulongojohnpaul@gmail.com>
2019-04-01 15:04:54 +0300

    refactor tests and add new test for Phrase#letter method

--- test/test.js
+++ test/test.js
@@ -23,9 +23,18 @@ describe("Phrase", function(){
             assert(punctuatedPalindrome.palindrome());
         });
 
+    });
+
+    describe('#letters', function(){
         it("should return only letters", function(){
             let punctuatedPalindrome = new Phrase("Madam, I'm Adam");
             assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
         });
+
+        it("should return the empty string on no match", function(){
+            let noLetters = new Phrase("1234.56");
+            assert.strictEqual(noLetters.letters(), "");
+        });
     });
+
 });
